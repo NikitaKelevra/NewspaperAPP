93c2210365c1230aad9042e359c9c94f068437b7
Никита <81251222+NikitaKelevra@users.noreply.github.com>
2022-03-26 17:17:49 +0300

    CollectionView building

---
+++ NewspaperAPP/NewsWeek/NewWeekCell.swift
@@ -0,0 +1,12 @@
+//
+//  NewWeekCell.swift
+//  NewspaperAPP
+//
+//  Created by Сперанский Никита on 26.03.2022.
+//
+
+import UIKit
+
+class NewsWeekCell: UICollectionViewCell {
+    
+}
--- NewspaperAPP/NewsWeekViewController.swift
+++ NewspaperAPP/NewsWeekViewController.swift
@@ -22,24 +22,12 @@ class NewsWeekViewController: UIViewController {
 
     }
     
+    
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+     
     }
-    */
 
 }
+
